e419d8f9e8a20ff53a275b5788f776e6cd0af520
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-01-20 16:01:13 +0200

    Completed Footer

--- Pages/Airport Taxis/airport.js
+++ Pages/Airport Taxis/airport.js
@@ -1,3 +1,10 @@
 // Enable Bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+var footerLinks = document.querySelectorAll("#footerLinks>div>div>ul>li>a")
+    .forEach(element => {
+        element.classList.add("text-decoration-none")
+    })
+
+    

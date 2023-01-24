ae95c6c37a34e325a6957817fe2a3fb4ec520087
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-01-25 01:22:56 +0200

    Add new page for details

---
+++ Pages/Airport Taxis/js/details.js
@@ -0,0 +1,3 @@
+// Enable Bootstrap tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))

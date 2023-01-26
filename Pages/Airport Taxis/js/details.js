0a63597ccfc4bd6903aa923c0f5fee416b918f5c
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-01-26 14:27:25 +0200

    Add alert and finish footer

--- Pages/Airport Taxis/js/details.js
+++ Pages/Airport Taxis/js/details.js
@@ -1,3 +1,9 @@
 // Enable Bootstrap tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Remove decoration(underline) from all anchor tags in footer
+var footerLinks = document.querySelectorAll("#footerLinks>div>ul>li>a")
+    .forEach(element => {
+        element.classList.add("text-decoration-none", "text-white", "fw-light")
+    })

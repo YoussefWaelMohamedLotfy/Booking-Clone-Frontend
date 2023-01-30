21e1add563a757e4a1e79ff8a2172ab9bf105ed7
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-01-30 22:13:29 +0200

    Display data in details from index

--- Pages/Airport Taxis/js/details.js
+++ Pages/Airport Taxis/js/details.js
@@ -6,6 +6,14 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 var footerLinks = document.querySelectorAll("#footerLinks>div>ul>li>a")
     .forEach(element => {
         element.classList.add("text-decoration-none", "text-white", "fw-light")
+
+        element.addEventListener("mouseenter", function (event) {
+            event.target.classList.remove("text-decoration-none")
+        })
+
+        element.addEventListener("mouseleave", function (event) {
+            event.target.classList.add("text-decoration-none")
+        })
     })
 
 // Parse query string to a dictionary
@@ -13,3 +21,6 @@ const queryString = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
 
+document.getElementById("from").innerText = queryString.onewayPickup
+document.getElementById("to").innerText = queryString.onewayDestination
+document.getElementById("passengersNumber").innerText = queryString.onewayNumPassengers

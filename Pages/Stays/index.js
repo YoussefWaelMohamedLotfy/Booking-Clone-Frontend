ca6fb4ae35ede187de7fb45aec1381538540bc25
Salsabeel <salsabeelhatem52@yahoo.com>
2023-01-31 21:41:20 +0200

    removed border of curr and languages & added hover

--- Pages/Stays/index.js
+++ Pages/Stays/index.js
@@ -79,8 +79,5 @@ for (var currency of currenciesMap) {
     buttonItem.appendChild(paragraphItem)
 
 
-    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+    
 }
